59afb6ed3ec7305c705474e7528506a2a6e7985e
James B <jabcobalt@gmail.com>
2024-02-03 21:20:42 +0000

    Move server.js to index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const http = require("https");
+const fs = require("fs");
+const stream = require("stream");
+const serveFile  = require("./serveFile.js");
+const ws = require("ws");
+
+const HTTPSServerOptions = {
+    key: fs.readFileSync("/etc/letsencrypt/live/natasha119.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/natasha119.com/fullchain.pem")
+};
+
+const server = http.createServer(HTTPSServerOptions);
+
+const wsserver = new ws.WebSocketServer({server: server, clientTracking: true});
+
+const ReplaceHurpDurp = new stream.Transform({
+    transform(chunk, encoding, callback)
+    {
+        callback(null, chunk.toString().replace(/a/g, "AAAASDKJF"));
+    }
+});
+
+server.on("request", function HTTPSServerRequest(req, res)
+{
+    serveFile(req, res, req.url);
+});
+
+wsserver.on("connection", function wssOnConnection(ws, req)
+{
+    ws.on("error", console.error);
+
+    ws.on("message", function wsOnMessage(data)
+    {
+        let dataJSON = JSON.parse(data);
+
+        if(dataJSON.mtype == "createMessage")
+        {
+            let messageJSON = {
+                mtype: "messageRecieved", 
+                message: "Somebody: " + dataJSON.message
+            }
+
+            wsserver.clients.forEach(socket => socket.send(JSON.stringify(messageJSON)));
+        }
+    });
+});
+
+server.listen(8822);
